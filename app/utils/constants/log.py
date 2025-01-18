6449829e4d9720090589c8123880a32e3defa921
haolamnm <haolamnm@gmail.com>
2025-01-19 00:35:41 +0700

    Refactor log file paths

--- app/utils/constants/log.py
+++ app/utils/constants/log.py
@@ -5,20 +5,30 @@ from typing import Final
 # Request Per Day (RPD)
 MAX_RPD: Final[int] = 1500
 RPD_LOG_FILE: Final[str] = os.path.join(
-	os.path.dirname(os.path.dirname(__file__)), 'app', 'logs', 'RPD.json'
+    os.path.dirname(
+	os.path.dirname(
+	os.path.dirname(os.path.abspath(__file__)))), 'logs', 'RPD.json'
 )
 
 # Request Per Minute (RPM)
 MAX_RPM: Final[int] = 10
 RPM_LOG_FILE: Final[str] = os.path.join(
-	os.path.dirname(os.path.dirname(__file__)), 'app', 'logs', 'RPM.json'
+    os.path.dirname(
+	os.path.dirname(
+	os.path.dirname(os.path.abspath(__file__)))), 'logs', 'RPM.json'
 )
 
 PROMPT_TEMPLATE_FILE: Final[str] = os.path.join(
-	os.path.dirname(os.path.dirname(__file__)), 'app', 'logs', 'prompt.txt'
+    os.path.dirname(
+	os.path.dirname(
+	os.path.dirname(os.path.abspath(__file__)))), 'logs', 'prompt.txt'
 )
 
-RESPONSE_LOG_FILE: Final[str] = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'app', 'logs', 'response.json')
+RESPONSE_LOG_FILE: Final[str] = os.path.join(
+    os.path.dirname(
+	os.path.dirname(
+	os.path.dirname(os.path.abspath(__file__)))), 'logs', 'response.json'
+)
 
 MAX_REASON_LENGTH: Final[int] = 500
 
