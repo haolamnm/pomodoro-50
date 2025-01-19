5227200b287f608645c1105880ed662214dd0001
haolamnm <haolamnm@gmail.com>
2025-01-19 13:34:56 +0700

    Add login_required decorator

---
+++ app/utils/decorators.py
@@ -0,0 +1,16 @@
+from functools import wraps
+from flask import flash, redirect, session, url_for
+
+
+def login_required(func):
+	@wraps(func)
+	def decorated_function(*args, **kwargs):
+		if session.get("user_id") is None:
+			flash('Please login to access this page', 'warning')
+			return redirect(url_for('login.index'))
+		return func(*args, **kwargs)
+
+	return decorated_function
+
+
+# def admin_required(func):
