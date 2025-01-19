0a81c5f04f84602febaf99d8433364649033cdf2
haolamnm <haolamnm@gmail.com>
2025-01-20 01:28:30 +0700

    Refactor error handling to use flash messages and redirects

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from typing import Type
-from flask import Flask, jsonify, url_for
+from flask import Flask, Response, url_for, flash, redirect
 from app.configs import Config
 from app.utils.extensions import (
 	db,
@@ -10,7 +10,7 @@ from app.utils.extensions import (
 from app.routes import (
 	init_routes,
 )
-from app.utils.types import RouteResponse
+from app.utils.types.route import RedirectResponse
 
 
 def create_app(config_class: Type[Config]) -> Flask:
@@ -26,24 +26,29 @@ def create_app(config_class: Type[Config]) -> Flask:
 
 	init_routes(app)
 	init_error_handlers(app)
+	init_after_request(app)
 
 	return app
 
 
 def init_error_handlers(app: Flask) -> None:
 	@app.errorhandler(404)
-	def handle_404_error(error) -> RouteResponse:
-		return jsonify({
-			'message': 'Page not found.',
-			'category': 'danger',
-			'redirect': url_for('home.index')
-		}), 404
+	def handle_404_error(error) -> RedirectResponse:
+		flash('Page not found', 'info')
+		return redirect(url_for('home.index')), 301
 
 	@app.errorhandler(500)
-	def handle_500_error(error) -> RouteResponse:
+	def handle_500_error(error) -> RedirectResponse:
 		"""Handle 500 Internal Server Error"""
-		return jsonify({
-			'message': 'Server error.',
-			'category': 'danger',
-			'redirect': url_for('home.index')
-		}), 500
+		flash('Internal server error', 'danger')
+		return redirect(url_for('home.index')), 301
+
+
+def init_after_request(app: Flask) -> None:
+	@app.after_request
+	def after_request(response: Response) -> Response:
+		""""This function ensures responses aren't cached."""
+		response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+		response.headers['Expires'] = 0
+		response.headers['Pragma'] = 'no-cache'
+		return response
