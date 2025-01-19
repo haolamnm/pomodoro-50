c22d3745c9a6ecd6bfc7e001289dd41c5af7940c
haolamnm <haolamnm@gmail.com>
2025-01-19 12:27:27 +0700

    Remove complete_password_required decorator from about route

--- app/routes/about.py
+++ app/routes/about.py
@@ -1,6 +1,6 @@
 from typing import Final
 from flask import Blueprint, render_template
-from app.utils.decorators import login_required, complete_password_required
+from app.utils.decorators import login_required
 
 
 about: Final[Blueprint] = Blueprint('about', __name__)
@@ -8,6 +8,5 @@ about: Final[Blueprint] = Blueprint('about', __name__)
 
 @about.route('/', methods=['GET'])
 @login_required
-@complete_password_required
 def index() -> str:
 	return render_template('about/index.html')
