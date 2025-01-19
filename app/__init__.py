5074d2fc78999eafcfb7d7f4750986b81c31250d
haolamnm <haolamnm@gmail.com>
2025-01-20 00:11:58 +0700

    Add error handling

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from typing import Type
-from flask import Flask
+from flask import Flask, jsonify, url_for
 from app.configs import Config
 from app.utils.extensions import (
 	db,
@@ -10,6 +10,7 @@ from app.utils.extensions import (
 from app.routes import (
 	init_routes,
 )
+from app.utils.types import RouteResponse
 
 
 def create_app(config_class: Type[Config]) -> Flask:
@@ -24,7 +25,25 @@ def create_app(config_class: Type[Config]) -> Flask:
 		db.create_all()
 
 	init_routes(app)
-
-	# TODO: Add error handling
+	init_error_handlers(app)
 
 	return app
+
+
+def init_error_handlers(app: Flask) -> None:
+	@app.errorhandler(404)
+	def handle_404_error(error) -> RouteResponse:
+		return jsonify({
+			'message': 'Page not found.',
+			'category': 'danger',
+			'redirect': url_for('home.index')
+		}), 404
+
+	@app.errorhandler(500)
+	def handle_500_error(error) -> RouteResponse:
+		"""Handle 500 Internal Server Error"""
+		return jsonify({
+			'message': 'Server error.',
+			'category': 'danger',
+			'redirect': url_for('home.index')
+		}), 500
