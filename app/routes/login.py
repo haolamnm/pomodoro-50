98a42cc61ab967ddb41e8cd96ee1e57a68d0a8ab
haolamnm <haolamnm@gmail.com>
2025-01-21 01:43:26 +0700

    Add GitHub login and Fix form names

--- app/routes/login.py
+++ app/routes/login.py
@@ -2,7 +2,7 @@ from typing import Final, Any
 from flask import current_app as app
 from flask import Blueprint, render_template, url_for, redirect, flash, session
 from app.models import User
-from app.forms.login import LoginEmailForm
+from app.forms.login import LoginEmailForm, LoginGoogleForm, LoginGitHubForm
 from app.utils.extensions import oauth
 from app.utils.exceptions import UserEmailNotFoundError
 from app.utils.types.route import RedirectResponse, RenderResponse
@@ -17,16 +17,33 @@ def email() -> RenderResponse | RedirectResponse:
 		flash('Logged in already', 'info')
 		return redirect(url_for('home.index')), 301
 
-	form: LoginEmailForm = LoginEmailForm()
-	if form.validate_on_submit():
-		form.user.set_session()
+	login_email_form: LoginEmailForm = LoginEmailForm()
+	login_google_form: LoginGoogleForm = LoginGoogleForm()
+	login_github_form: LoginGitHubForm = LoginGitHubForm()
+
+	if (login_email_form.validate_on_submit()
+	 	and login_email_form.form_name.data == 'login_email_form'):
+		login_email_form.user.set_session()
 		flash('Logged in successfully', 'success')
 		return redirect(url_for('home.index')), 301
 
-	return render_template('login.html', form=form), 200
+	if (login_google_form.validate_on_submit()
+	 	and login_google_form.form_name.data == 'login_google_form'):
+		return redirect(url_for('login.google')), 302
+
+	if (login_github_form.validate_on_submit()
+	 	and login_github_form.form_name.data == 'login_github_form'):
+		return redirect(url_for('login.github')), 302
+
+	return render_template(
+		'login.html',
+		login_email_form=login_email_form,
+		login_google_form=login_google_form,
+		login_github_form=login_github_form,
+	), 200
 
 
-@login.route('/google', methods=['GET'])
+@login.route('/google', methods=['GET', 'POST'])
 def google() -> RedirectResponse:
 	try:
 		redirect_uri: str = url_for('login.google_authorize', _external=True)
@@ -38,7 +55,7 @@ def google() -> RedirectResponse:
 	except (ValueError, Exception) as e:
 		flash('Logged in with Google failed', 'danger')
 		app.logger.error(f'[ERROR] {e}')
-		return redirect(url_for('login.index')), 302
+		return redirect(url_for('login.email')), 302
 
 
 @login.route('/google/authorize', methods=['GET'])
@@ -63,7 +80,7 @@ def google_authorize() -> RedirectResponse:
 			user.update()
 
 		user.set_session()
-		flash('Logged in successfully', 'success')
+		flash('Logged in with Google successfully', 'success')
 		return redirect(url_for('home.index')), 302
 
 	except UserEmailNotFoundError:
@@ -75,10 +92,66 @@ def google_authorize() -> RedirectResponse:
 		)
 		new_user.create()
 		new_user.set_session()
-		flash('Logged in successfully', 'success')
+		flash('Signed up with Google successfully', 'success')
 		return redirect(url_for('home.index')), 302
 
 	except (ValueError, Exception) as e:
 		flash('Authorization with Google failed', 'danger')
+		app.logger.error(f'[ERROR] {e}')
+		return redirect(url_for('login.email')), 302
+
+
+@login.route('/github', methods=['POST'])
+def github() -> RedirectResponse:
+	try:
+		redirect_uri: str = url_for('login.github_authorize', _external=True)
+		client: Any = oauth.create_client('github')
+		if client is None:
+			raise ValueError('Failed to create OAuth client for GitHub')
+		return client.authorize_redirect(redirect_uri), 302
+
+	except (ValueError, Exception) as e:
+		flash('Logged in with GitHub failed', 'danger')
+		app.logger.error(f'[ERROR] {e}')
+		return redirect(url_for('login.email')), 302
+
+
+@login.route('/github/authorize', methods=['GET'])
+def github_authorize() -> RedirectResponse:
+	try:
+		client: Any = oauth.create_client('github')
+		if client is None:
+			raise ValueError('Failed to create OAuth client for GitHub')
+		client.authorize_access_token()
+		response = client.get('user').json()
+
+		email: Final[str] = response['email']
+		oauth_token: Final[str] = response['id']
+		oauth_provider: Final[str] = 'github'
+
+		user: User = User.get_by_email(email)
+		if user.oauth_provider != oauth_provider or user.oauth_token != oauth_token:
+			user.oauth_token = oauth_token
+			user.oauth_provider = oauth_provider
+			user.update()
+
+		user.set_session()
+		flash('Logged in with GitHub successfully', 'success')
+		return redirect(url_for('home.index')), 302
+
+	except UserEmailNotFoundError:
+		new_user: User = User(
+			email=email,
+			raw_password=None,
+			oauth_token=oauth_token,
+			oauth_provider=oauth_provider
+		)
+		new_user.create()
+		new_user.set_session()
+		flash('Signed up with GitHub successfully', 'success')
+		return redirect(url_for('home.index')), 302
+
+	except (ValueError, Exception) as e:
+		flash('Authorization with GitHub failed', 'danger')
 		app.logger.error(f'[ERROR] {e}')
 		return redirect(url_for('login.email')), 302
