cbadb9e295fc995c800e45310fecf0a937a0681e
haolamnm <haolamnm@gmail.com>
2025-01-20 20:29:50 +0700

    Update home route

--- app/routes/home.py
+++ app/routes/home.py
@@ -5,7 +5,7 @@ from flask import Blueprint, render_template
 home: Final[Blueprint] = Blueprint('home', __name__)
 
 
-@home.route('/home', methods=['GET'])
 @home.route('/', methods=['GET'])
+@home.route('/home', methods=['GET'])
 def index() -> str:
-	return render_template('home/index.html')
+	return render_template('home.html')
