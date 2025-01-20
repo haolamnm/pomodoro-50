6d365d69f04ed40d6e99a27e54e7f88a73cb2095
haolamnm <haolamnm@gmail.com>
2025-01-20 23:18:11 +0700

    Remove timer route

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -3,7 +3,6 @@ from flask import Flask
 from app.routes.core import core
 from app.routes.home import home
 from app.routes.about import about
-from app.routes.timer import timer
 from app.routes.login import login
 from app.routes.logout import logout
 from app.routes.signup import signup
@@ -17,7 +16,6 @@ def init_routes(app: Flask) -> None:
 	app.register_blueprint(home, url_prefix='/')
 	app.register_blueprint(core, url_prefix='/core')
 	app.register_blueprint(about, url_prefix='/about')
-	app.register_blueprint(timer, url_prefix='/timer')
 	app.register_blueprint(login, url_prefix='/login')
 	app.register_blueprint(logout, url_prefix='/logout')
 	app.register_blueprint(signup, url_prefix='/signup')
