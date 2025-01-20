70c523ea0d1a5650a5ac2c57ccf400ce6f550c83
haolamnm <haolamnm@gmail.com>
2025-01-20 19:47:00 +0700

    Add login with email

--- app/forms/login/email.py
+++ app/forms/login/email.py
@@ -1,18 +1,36 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, ValidationError
 from wtforms.validators import DataRequired, Email
+from app.models.user import User
+from app.utils.exceptions.user import UserEmailNotFoundError
 
 
 class LoginEmailForm(FlaskForm):
 	email: StringField = StringField(
 		label='Email',
-		validators=[DataRequired(), Email()]
+		validators=[
+			DataRequired(message='Email is required'),
+			Email(message='Email is invalid')
+		]
 	)
 	password: PasswordField = PasswordField(
 		label='Password',
-		validators=[DataRequired()]
+		validators=[
+			DataRequired(message='Password is required')
+		]
 	)
 	submit: SubmitField = SubmitField(
 		label='Email login',
 		render_kw={"class": "btn btn-primary", "style": "width: 100%;"}
 	)
+
+	def validate_email(self, field):
+		try:
+			user: User = User.get_by_email(field.data)
+			self.user = user
+		except UserEmailNotFoundError:
+			raise ValidationError('Email is not signed up')
+
+	def validate_password(self, field):
+		if hasattr(self, 'user') and not self.user.verify_password(field.data):
+			raise ValidationError('Password is incorrect')
---
+++ app/routes/login.py
@@ -0,0 +1,23 @@
+from typing import Final
+from flask import Blueprint, render_template, url_for, redirect, flash, session
+from app.forms.login import LoginEmailForm
+from app.utils.types.route import RedirectResponse, RenderResponse
+
+
+login: Final[Blueprint] = Blueprint('login', __name__)
+
+
+@login.route('/', methods=['GET', 'POST'], endpoint='index')
+@login.route('/email', methods=['GET', 'POST'])
+def email() -> RenderResponse | RedirectResponse:
+	if 'user_id' in session:
+		flash('Logged in already', 'info')
+		return redirect(url_for('home.index')), 301
+
+	form: LoginEmailForm = LoginEmailForm()
+	if form.validate_on_submit():
+		form.user.set_session()
+		flash('Logged in successfully', 'success')
+		return redirect(url_for('home.index')), 301
+
+	return render_template('login/index.html', form=form), 200
