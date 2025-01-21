37f70a1d816c396d06a09d85df2b63e390071a13
haolamnm <haolamnm@gmail.com>
2025-01-21 19:15:06 +0700

    Add update email form

--- app/forms/profile/update_email.py
+++ app/forms/profile/update_email.py
@@ -1,5 +1,47 @@
 from flask_wtf import FlaskForm
+from wtforms.fields import SubmitField, StringField, PasswordField
+from wtforms.validators import DataRequired, Email, Length, ValidationError
+from app.models.user import User
+from app.utils.exceptions.user import UserEmailNotFoundError
 
 
 class UpdateEmailForm(FlaskForm):
-	pass
+	new_email: StringField = StringField(
+		label='New Email',
+		validators=[
+			DataRequired(message='New Email is required'),
+			Email(message='New Email is invalid'),
+			Length(
+				max=255,
+				message='New Email must be less than 255 characters'
+			)
+		]
+	)
+	password: PasswordField = PasswordField(
+		label='Password',
+		validators=[
+			DataRequired(message='Password is required')
+		]
+	)
+	submit: SubmitField = SubmitField(
+		label='Update Email',
+		render_kw={"class": "btn btn-primary", "style": "width: 100%;"}
+	)
+
+	def __init__(self, user: User, *args, **kwargs) -> None:
+		super().__init__(*args, **kwargs)
+		self.user = user
+
+	def validate_new_email(self, field) -> None:
+		if field.data == self.user.email:
+			raise ValidationError('Email is the same as the current email')
+
+		try:
+			User.get_by_email(field.data)
+			raise ValidationError('Email is already signed up')
+		except UserEmailNotFoundError:
+			pass
+
+	def validate_password(self, field) -> None:
+		if hasattr(self, 'user') and not self.user.verify_password(field.data):
+			raise ValidationError('Password is incorrect')
