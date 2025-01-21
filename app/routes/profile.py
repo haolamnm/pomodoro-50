4a72074381c8af826edbfd5dda4b085ded8e07b0
haolamnm <haolamnm@gmail.com>
2025-01-21 19:15:21 +0700

    Add update email route

--- app/routes/profile.py
+++ app/routes/profile.py
@@ -1,8 +1,9 @@
 from typing import Final
 from flask import Blueprint, flash, redirect, url_for, render_template, session
 from flask_mail import Message
-from app.forms.profile import ResetPasswordRequestForm, ResetPasswordTokenForm
+from app.forms.profile import ResetPasswordRequestForm, ResetPasswordTokenForm, UpdateEmailForm, UpdatePasswordForm
 from app.models.user import User
+from app.utils.decorators import login_required
 from app.utils.extensions import mail
 from app.utils.types.route import RenderResponse, RedirectResponse
 
@@ -10,11 +11,29 @@ from app.utils.types.route import RenderResponse, RedirectResponse
 profile: Final[Blueprint] = Blueprint('profile', __name__)
 
 
-# @profile.route('/<string:username>', methods=['GET'])
-# def index(username: str):
-# 	return f'Profile: {username}'
+@profile.route('/', methods=['GET'])
+@profile.route('/me', methods=['GET'])
+@login_required
+def me() -> RenderResponse:
+	return render_template('profile/me.html'), 200
 
-@profile.route('reset/password/request', methods=['GET', 'POST'])
+
+@profile.route('/update/email', methods=['GET', 'POST'])
+@login_required
+def update_email() -> RenderResponse | RedirectResponse:
+	user: User = User.get_by_id(session['user_id'])
+	form: UpdateEmailForm = UpdateEmailForm(user=user)
+	if form.validate_on_submit():
+		form.user.email = str(form.new_email.data)
+		form.user.update()
+		form.user.set_session()
+		flash('Email updated successfully', 'success')
+		return redirect(url_for('profile.me')), 301
+
+	return render_template('profile/update_email.html', form=form), 200
+
+
+@profile.route('/reset/password/request', methods=['GET', 'POST'])
 def reset_password_request() -> RenderResponse | RedirectResponse:
 	if 'user_id' in session:
 		flash('Logged in already', 'info')
@@ -33,7 +52,7 @@ def reset_password_request() -> RenderResponse | RedirectResponse:
 	), 200
 
 
-@profile.route('reset/password/<string:token>', methods=['GET', 'POST'])
+@profile.route('/reset/password/<string:token>', methods=['GET', 'POST'])
 def reset_password_token(token: str) -> RenderResponse | RedirectResponse:
 	try:
 		user: User = User.verify_token(token)
