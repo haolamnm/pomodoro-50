94b4c3493dcf93dda4e369840829f3d967cf9186
haolamnm <haolamnm@gmail.com>
2025-01-22 21:34:57 +0700

    Add modal to gather reason

---
+++ app/static/js/timer.js
@@ -0,0 +1,80 @@
+document.addEventListener("DOMContentLoaded", function () {
+	let timer;
+	const timerDisplay = document.getElementById("timer-display");
+
+	function convertTimeToSeconds(time) {
+		const [minutes, seconds] = time.split(":");
+		return parseInt(minutes) * 60 + parseInt(seconds);
+	}
+
+	let remainingTime = convertTimeToSeconds(timerDisplay.textContent);
+	console.log(remainingTime);
+	let isRunning = false;
+
+	const startButton = document.getElementById("start-button");
+	const pauseButton = document.getElementById("pause-button");
+
+	function formatTime(seconds) {
+		const minutes = Math.floor(seconds / 60).toString().padStart(2, "0");
+		const secs = (seconds % 60).toString().padStart(2, "0");
+		return `${minutes}:${secs}`;
+	}
+
+	function updateDisplay() {
+		timerDisplay.textContent = formatTime(remainingTime);
+	}
+
+	function startTimer() {
+		if (!isRunning) {
+			isRunning = true;
+			startButton.classList.add("d-none");
+			pauseButton.classList.remove("d-none");
+
+			timer = setInterval(() => {
+				if (remainingTime > 0) {
+					remainingTime -= 1;
+					updateDisplay();
+				} else {
+					clearInterval(timer);
+					isRunning = false;
+					alert("Time's up!"); // TODO: Update database
+					startButton.classList.remove("d-none");
+					pauseButton.classList.add("d-none");
+
+					// TODO: Fetch from database
+					// Like document.getElementById("custom-pomodoro-time");
+					// const customPomodoroTime = 50 * 60;
+				}
+			}, 1000);
+		}
+	}
+
+	function pauseTimer() {
+		if (isRunning) {
+			isRunning = false;
+			clearInterval(timer);
+			startButton.classList.remove("d-none");
+			pauseButton.classList.add("d-none");
+		}
+	}
+
+	startButton.addEventListener("click", startTimer);
+	pauseButton.addEventListener("click", pauseTimer);
+
+	const stopModal = new bootstrap.Modal(document.getElementById("stopModal"));
+	const confirmStop = document.getElementById("confirm-stop");
+
+	pauseButton.addEventListener("click", () => {
+		// console.log(remainingTime);
+		stopModal.show();
+	});
+
+	confirmStop.addEventListener("click", () => {
+		stopModal.hide();
+		pauseTimer();
+		console.log(remainingTime);
+		remainingTime = convertTimeToSeconds(timerDisplay.textContent); // Reset timer
+		console.log(remainingTime);
+		updateDisplay();
+	});
+});
