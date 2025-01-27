d714bf60dc3d17ffa19f5dc3e09e1a7f245d8ae0
haolamnm <haolamnm@gmail.com>
2025-01-27 20:24:46 +0700

    Refactor history route

--- app/routes/history.py
+++ app/routes/history.py
@@ -1,6 +1,7 @@
 from typing import Final
 from flask import Blueprint, render_template, session
 from app.models import Pomodoro
+from app.utils.types import RenderResponse
 from app.utils.decorators import login_required
 
 
@@ -10,8 +11,8 @@ history: Final[Blueprint] = Blueprint('history', __name__)
 @history.route('/', methods=['GET'])
 @history.route('/me', methods=['GET'])
 @login_required
-def me() -> str:
+def me() -> RenderResponse:
 	user_id: int = session['user_id']
 	pomodoros: list[Pomodoro] = Pomodoro.get_by_user_id(user_id, 'desc')
 
-	return render_template('history.html', pomodoros=pomodoros)
+	return render_template('history.html', pomodoros=pomodoros), 200
