9feb4edb06bef559586c84941a88699b2f271d84
haolamnm <haolamnm@gmail.com>
2025-01-27 21:03:31 +0700

    Add backend to handle heatmap data

--- app/utils/helpers.py
+++ app/utils/helpers.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+from app.models import Pomodoro
+from app.utils.types import DurationLog
 
 
 def get_weekly_duration(durations: list[int], start_ats: list[datetime]) -> list[float]:
@@ -14,3 +16,18 @@ def get_weekly_duration(durations: list[int], start_ats: list[datetime]) -> list
 
 	return weekly_durations
 
+
+def get_all_time_duration(pomodoros: list[Pomodoro]) -> list[DurationLog]:
+	date_duration_logs: dict[str, float] = {}
+
+	for pomodoro in pomodoros:
+		date: str = pomodoro.start_at.strftime('%Y-%m-%d')
+		duration: float = round(pomodoro.duration / 60, 2) # Convert seconds to minutes
+		date_duration_logs[date] = date_duration_logs.get(date, 0) + duration
+
+	all_time_duration_logs: list[DurationLog] = [
+		{'date': date, 'value': duration}
+		for date, duration in date_duration_logs.items()
+	]
+
+	return all_time_duration_logs
--- app/utils/types/__init__.py
+++ app/utils/types/__init__.py
@@ -1,3 +1,4 @@
 from app.utils.types.core import *
 from app.utils.types.route import *
 from app.utils.types.session import *
+from app.utils.types.progress import *
