6aa457ed05735a5d1c3bf05e98f252b170ef4cf2
haolamnm <haolamnm@gmail.com>
2025-01-27 20:23:27 +0700

    Add about route

--- app/routes/about.py
+++ app/routes/about.py
@@ -1,12 +1,14 @@
 from typing import Final
 from flask import Blueprint, render_template
 from app.utils.decorators import login_required
+from app.utils.types import RenderResponse
 
 
 about: Final[Blueprint] = Blueprint('about', __name__)
 
 
 @about.route('/', methods=['GET'])
+@about.route('/app', methods=['GET'])
 @login_required
-def index() -> str:
-	return render_template('about/index.html')
+def app() -> RenderResponse:
+	return render_template('about.html'), 200
